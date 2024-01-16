48fcc17317a67f3a41318b3e24199e4336c96c04
Manuel Lucero <cidorbleit@gmail.com>
2024-01-16 10:38:02 -0500

    go

--- esbuild.dev.cjs
+++ esbuild.dev.cjs
@@ -1,7 +1,9 @@
 require('esbuild').build({
-    entryPoints: ['src/index.tsx'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outfile: 'dist/bundle.js'
-  }).catch(() => process.exit(1));
+  entryPoints: ['src/index.tsx'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outfile:'./dist/bundle.js',
+  target: 'es2016', // Alinea el target con el de tsconfig.json
+  format: 'cjs', // Utiliza el formato CommonJS
+}).catch(() => procces.exit(1));
