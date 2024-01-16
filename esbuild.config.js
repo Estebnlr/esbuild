c933296d0f82b2c48ce3f9b4cc18e7b65ea7dcfa
Manuel Lucero <cidorbleit@gmail.com>
2024-01-16 11:01:00 -0500

    go

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,11 @@
 import esbuildServe from 'esbuild-serve';
 
 esbuildServe({
-  entryPoints: ['src/index.tsx'],
+  entryPoints: ['./src/index.tsx'],
   bundle: true,
   minify: true,
   sourcemap: true,
-  outfile: './public/bundle.js',
+  outfile: 'dist/bundle.js',
   define: {
     'process.env.NODE_ENV': '"development"',
     'process.env.DEBUG': 'false'
--- esbuild.dev.cjs
+++ esbuild.dev.cjs
@@ -1,9 +1,9 @@
 require('esbuild').build({
-  entryPoints: ['src/index.tsx'],
+  entryPoints: ['./src/index.tsx'],
   bundle: true,
   minify: true,
   sourcemap: true,
-  outfile:'./public/bundle.js',
+  outfile: 'dist/bundle.js',
   target: 'es2016', // Alinea el target con el de tsconfig.json
   format: 'cjs', // Utiliza el formato CommonJS
 }).catch(() => procces.exit(1));
